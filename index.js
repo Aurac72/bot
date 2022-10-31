8e425d4b6c08060742989cf82df25cb1941f4965
Aurac72 <117123618+Aurac72@users.noreply.github.com>
2022-11-01 00:08:02 +0100

    update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client({DisableEveryone: true});
 
 bot.on('ready', async () =>{
-  bot.user.setActivity('bot. BOnjour!');
+  bot.user.setActivity('');
 });
 
 bot.login(process.env.Bot_Token)
